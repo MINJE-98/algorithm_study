8cf9603831cdfa389dd13d7567b33a74dff91898
조민제 <56459078+MINJE-98@users.noreply.github.com>
2021-11-04 12:44:00 +0000

    readme 수정

--- Python/main.py
+++ Python/main.py
@@ -2,6 +2,8 @@ import sys
 # sys.path.append('/home/runner/algorithmstudy/Python/Programmers/Sort')
 # import numberOfK
 
-sys.path.append('/home/runner/algorithmstudy/Python/Factorial')
-import recursive
+# sys.path.append('/home/runner/algorithmstudy/Python/Factorial')
+# import recursive
 
+sys.path.append('/home/runner/algorithmstudy/Python/100문제')
+import five
